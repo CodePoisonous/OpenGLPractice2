654d084a12984da3656a22ad0a3595faf67f8299
chuyanht <379482455@qq.com>
2023-04-24 18:14:28 +0800

    mesh

--- src/Model.cpp
+++ src/Model.cpp
@@ -37,7 +37,7 @@ void Model::loadModel(const string& path)
 		return;
 	}
 
-	//m_directory = path.substr(0, path.find_last_of('/'));
+	m_directory = path.substr(0, path.find_last_of('/'));
 
 	processNode(scene->mRootNode, scene);
 }
@@ -65,6 +65,46 @@ Mesh Model::processMesh(const aiMesh* mesh, const aiScene* scene)
 	vector<Vertex> vertices;
 	vector<unsigned int> indices;
 	vector<Texture> textures;
+
+	// 顶点数据
+	for (unsigned int i = 0; i < mesh->mNumVertices; ++i)
+	{
+		Vertex vx;
+		// 顶点位置
+		vx.m_Position.x = mesh->mVertices[i].x;
+		vx.m_Position.y = mesh->mVertices[i].y;
+		vx.m_Position.z = mesh->mVertices[i].z;
+
+		// 法线
+		vx.m_Normal.x = mesh->mNormals[i].x;
+		vx.m_Normal.y = mesh->mNormals[i].y;
+		vx.m_Normal.z = mesh->mNormals[i].z;
+
+		// 纹理坐标
+		if (mesh->mTextureCoords[0])
+		{
+			vx.m_TexCoords.x = mesh->mTextureCoords[0][i].x;
+			vx.m_TexCoords.y = mesh->mTextureCoords[0][i].y;
+		}
+		else vx.m_TexCoords = glm::vec2(0.0f, 0.0f);
+
+		vertices.push_back(vx);
+	}
+
+	// 网格索引
+	for (unsigned int i = 0; i < mesh->mNumFaces; ++i)
+	{
+		aiFace face = mesh->mFaces[i];
+		for (unsigned int j = 0; j < face.mNumIndices; ++j)
+			indices.push_back(face.mIndices[j]);
+	}
+
+	// 材质
+	if (mesh->mMaterialIndex >= 0)
+	{
+
+	}
+
 	return Mesh(vertices, indices, textures);
 }
 
